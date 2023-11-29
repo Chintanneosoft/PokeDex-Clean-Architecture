03502af0590b9c143ca125115c73824ef57538b8
Chintanneosoft <chintan.rajgor@neosoftmail.com>
2023-11-29 12:53:09 +0530

    Initial Commit

---
+++ PokeDex/PokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexApp.swift
+//  PokeDex
+//
+//  Created by Neosoft on 29/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
